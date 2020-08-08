ecc4d34c31ca8045fdb2e3284adb017d30fbfedc
NCarrez <NCarrez@users.noreply.github.com>
2020-08-08 18:00:36 +0200

    Mise a jour de la UI test

--- projects/master_manager/sources/ui.py
+++ projects/master_manager/sources/ui.py
@@ -7,11 +7,12 @@ if not (file_path in sys.path):
 
 from PySide2.QtGui import QKeySequence
 from PySide2.QtCore import Signal, Slot
-from PySide2.QtWidgets import QApplication, QMainWindow, QWidget
-from PySide2.QtWidgets import QGridLayout, QVBoxLayout
+from PySide2.QtWidgets import QApplication, QMainWindow, QGroupBox, QWidget
+from PySide2.QtWidgets import QGridLayout, QVBoxLayout, QHBoxLayout
 from PySide2.QtWidgets import QLineEdit, QPushButton, QLabel
 from PySide2.QtWidgets import QAction, QShortcut
 from PySide2.QtWidgets import QListWidget, QListWidgetItem
+from PySide2.QtWidgets import QFrame, QStyle
 
 from applications_skeleton import my_app
 
@@ -30,7 +31,14 @@ def make_adminlist_item(username):
 def build_adminlistwidget():
     list_widget = QListWidget()
     list_widget.items = []
-    users = [["admin", "admin"], ["admin1", "admin1"]]
+    users = [
+        ["admin", "admin"],
+        ["admin1", "admin1"],
+        ["admin2", "admin2"],
+        ["admin3", "admin3"],
+        ["admin4", "admin4"],
+        ["admin5", "admin5"],
+    ]
 
     for username, password in users:
         item = make_adminlist_item(username)
@@ -44,12 +52,77 @@ def build_adminlistwidget():
     pass
 
 
-def build_centralwidget(widget):
-    widget.layout = QGridLayout()
+def build_adminpanelwidget():
+    widget = QFrame()
+    widget.setFrameStyle(QFrame.Panel | QFrame.Raised)
+    # widget = QWidget()
+    # widget.setStyleSheet(
+    #    "background-color: rgb(255,0,0); margin:5px; border:1px solid rgb(0, 255, 0); "
+    # )
+    # widget.setStyle(QStyle.PE_FrameWindow)
+    widget.layout = QVBoxLayout()
+    widget.we_username = QLineEdit("username")
+    widget.wl_username = QLabel("username")
+    widget.wl_username.setBuddy(widget.we_username)
+    widget.we_username.setReadOnly(True)
 
-    widget.layout.admin_list_widget = build_adminlistwidget()
-    widget.layout.addWidget(widget.layout.admin_list_widget)
+    widget.pas_layout = QHBoxLayout()
+    widget.we_password = QLineEdit("password")
+    widget.wl_password = QLabel("password")
+    widget.wl_password.setBuddy(widget.we_password)
+    widget.we_password.setReadOnly(True)
+    widget.we_password.setEchoMode(QLineEdit.Password)
+    widget.reveal = QPushButton("Reveal")
+
+    widget.btn_layout = QHBoxLayout()
+    widget.update = QPushButton("Update")
+    widget.delete = QPushButton("Delete")
+
+    widget.layout.addWidget(widget.wl_username)
+    widget.layout.addWidget(widget.we_username)
+    widget.layout.addWidget(widget.wl_password)
+    widget.layout.insertLayout(-1, widget.pas_layout)
+    widget.pas_layout.addWidget(widget.we_password)
+    widget.pas_layout.addWidget(widget.reveal)
+    widget.layout.addStretch()
+    widget.layout.insertLayout(-1, widget.btn_layout)
+    widget.btn_layout.addWidget(widget.update)
+    widget.btn_layout.addWidget(widget.delete)
     widget.setLayout(widget.layout)
+    return widget
+    pass
+
+
+def build_adminaddwidget():
+    widget = QWidget()
+    widget.layout = QVBoxLayout()
+    widget.add = QPushButton("Add")
+
+    widget.layout.addWidget(widget.add)
+    widget.setLayout(widget.layout)
+    return widget
+    pass
+
+
+def build_centralwidget(widget):
+    widget.mlayout = QHBoxLayout()
+    widget.gbox = QGroupBox("Admin Manager")
+    widget.mlayout.addWidget(widget.gbox)
+    widget.layout = QHBoxLayout()
+
+    widget.lst_layout = QVBoxLayout()
+    widget.layout.admin_list_widget = build_adminlistwidget()
+    widget.lst_layout.addWidget(widget.layout.admin_list_widget)
+    widget.layout.admin_add_widget = build_adminaddwidget()
+    widget.lst_layout.addWidget(widget.layout.admin_add_widget)
+    widget.layout.insertLayout(-1, widget.lst_layout)
+
+    widget.layout.insertSpacing(-1, 5)
+
+    widget.layout.admin_panel_widget = build_adminpanelwidget()
+    widget.layout.addWidget(widget.layout.admin_panel_widget)
+    widget.gbox.setLayout(widget.layout)
+    widget.setLayout(widget.mlayout)
     pass
 
 
