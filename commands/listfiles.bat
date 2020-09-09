1c509150c93e1042f6f34c2653053c831c2cff76
Nicolas CARREZ <ncarrez@imagine-optic.com>
2020-09-09 08:26:55 +0200

    Ajout de commentaires pour listfiles.bat

--- commands/listfiles.bat
+++ commands/listfiles.bat
@@ -1,6 +1,10 @@
 @echo off
 
+:: Recuperation du chemin vers le dossier du fichier
 set this_file_path=%~dp0 
+:: Suppression de l'espace de fin
 set this_file_path=%this_file_path: =%
+
 echo.
+:: Appel du script python et passage de tous les parametres avec "%*"
 python "%this_file_path%_listfiles.py" %*
