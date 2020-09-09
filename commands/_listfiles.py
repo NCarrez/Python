75d3ddd0dc0712add654d36bb8a2d5e66c0800f7
Nicolas CARREZ <ncarrez@imagine-optic.com>
2020-09-09 08:36:36 +0200

    Mise a jour des snippets commentaires, ajout de commentaires pour _listfiles.py

--- commands/_listfiles.py
+++ commands/_listfiles.py
@@ -4,6 +4,16 @@ import sys, os
 
 
 def print_files(path):
+    """
+    Function Descrition :
+    Print file found from given path
+
+    Function Arguments : 
+    path : The full path to print files from
+
+    Function Return : 
+    None : None
+    """
     res = fe.list_files(path)
     for line in res:
         print(line)
@@ -17,11 +27,13 @@ if __name__ == "__main__":
     if args[1:] != []:
         paths = args[1:]
 
+    ### if not a list, transform to list
     if not isinstance(paths, list):
         t_path = paths
         paths = []
         paths.append(t_path)
 
+    ### for each path prints the files
     for path in paths:
         if path[-1] != "\\":
             path = path + "\\"
